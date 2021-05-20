e7aa535ee0f88925f69fa3dfdb6bb66326f0f237
Jordan Mele <SiliconSoldier@outlook.com.au>
2021-05-20 17:28:20 +1000

    Correction to worker making worker scenario tests

--- test/fixtures/worker-making-worker.cjs
+++ test/fixtures/worker-making-worker.cjs
@@ -2,5 +2,9 @@ const Worker = require('web-worker');
 
 const worker = new Worker(require.resolve('./worker.cjs'));
 worker.addEventListener('message', e => {
-    postMessage(e);
+    postMessage(e.data);
+});
+
+addEventListener('message', e => {
+    worker.postMessage(e.data);
 });
