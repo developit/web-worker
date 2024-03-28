a8c03807c1ea00b919d24ead717980ddd2492904
martines3000 <domajnko.martin@gmail.com>
2024-03-28 21:49:08 +0100

    fix: resolve lint issues, fix import in tests

--- tsup.config.js
+++ tsup.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
-  target: 'es2020',
-  treeshake: false,
-  splitting: false,
-  entry: ['src/**/*.js'],
-  format: ['cjs'],
-  dts: false,
-  minify: false,
-  clean: false,
-  shims: true,
-  ...options,
+	target: 'es2020',
+	treeshake: false,
+	splitting: false,
+	entry: ['src/**/*.js'],
+	format: ['cjs'],
+	dts: false,
+	minify: false,
+	clean: false,
+	shims: true,
+	...options
 }));
